0096acc4f617a7787fb00104110535affd4cd3dc
Snehil Kumar <snehil.kumar09@gmail.com>
2023-07-20 08:40:23 +0530

    Time: 29 ms (5.77%), Space: 17.8 MB (24.17%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    bool sameParity(int a, int b){
+        if((a<0 && b<0) || (a>0 && b>0)) return true;
+        return false;
+    }
+public:
+    vector<int> asteroidCollision(vector<int>& v) {
+        int n=v.size();
+        stack<int> st;
+        for(int i=0;i<n;i++){
+            if(st.size()==0 || sameParity(st.top(),v[i]) || (st.top()<0 && v[i]>0)){
+                st.push(v[i]); // no collision
+            }else{
+                while(st.size()>0 && st.top()>0 && st.top() < abs(v[i])) st.pop();\
+                if(st.size()==0 || st.top()<0){
+                    st.push(v[i]);
+                }else if(st.top() == abs(v[i])){
+                    st.pop();
+                }
+            }
+        }
+        vector<int> ans;
+        while(st.size()>0){
+            int x=st.top();
+            ans.push_back(x);
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
