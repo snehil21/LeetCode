e1c6ee526dfed93faae7b16f9b52222f02af8d01
Snehil Kumar <snehil.kumar09@gmail.com>
2023-08-13 22:58:06 +0530

    Added solution - LeetHub

---
+++ Nth Fibonacci Number - GFG/nth-fibonacci-number.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    int nthFibonacci(int n){
+        long long int first=0,second=1;
+        n--;
+        while(n--){
+            long long int sum=(first+second)%1000000007;
+            first=second;
+            second=sum;
+        }
+        return (int)second;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.nthFibonacci(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
