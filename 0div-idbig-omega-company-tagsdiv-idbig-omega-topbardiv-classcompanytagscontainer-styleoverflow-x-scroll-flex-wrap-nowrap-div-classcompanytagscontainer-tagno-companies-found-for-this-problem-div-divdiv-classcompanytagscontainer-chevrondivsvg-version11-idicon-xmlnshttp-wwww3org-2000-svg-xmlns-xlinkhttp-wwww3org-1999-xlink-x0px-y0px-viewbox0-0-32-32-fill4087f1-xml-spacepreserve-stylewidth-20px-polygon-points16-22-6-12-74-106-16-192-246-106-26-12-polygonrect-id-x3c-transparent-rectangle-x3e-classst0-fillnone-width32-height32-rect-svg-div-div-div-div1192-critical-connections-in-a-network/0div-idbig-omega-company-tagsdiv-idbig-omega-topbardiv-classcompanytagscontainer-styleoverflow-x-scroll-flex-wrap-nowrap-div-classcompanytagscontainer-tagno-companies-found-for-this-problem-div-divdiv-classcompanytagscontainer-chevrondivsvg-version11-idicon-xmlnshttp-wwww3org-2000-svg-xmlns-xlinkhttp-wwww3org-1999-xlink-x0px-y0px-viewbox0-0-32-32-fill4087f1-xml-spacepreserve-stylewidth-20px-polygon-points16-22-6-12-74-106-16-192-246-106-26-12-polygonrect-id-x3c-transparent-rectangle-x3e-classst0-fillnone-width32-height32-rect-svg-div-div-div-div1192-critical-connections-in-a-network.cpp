1dc8cadcc0dc52ea367fc9234fdc3cb3e04572dc
Snehil Kumar <snehil.kumar09@gmail.com>
2023-09-30 16:02:40 +0530

    Time: 460 ms (64.75%), Space: 173.7 MB (61.30%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1192-critical-connections-in-a-network/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1192-critical-connections-in-a-network.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    int timer=1;
+private:
+    void dfs(int node,int parent,vector<int>adj[],vector<int>&vis,int tin[],int low[],vector<vector<int>>&bridges){
+        vis[node]=1;
+        tin[node]=low[node]=timer++;
+        for(auto it:adj[node]){
+            if(it==parent) continue;
+            if(!vis[it]){
+                dfs(it,node,adj,vis,tin,low,bridges);
+                low[node]=min(low[node],low[it]);
+                if(low[it]>tin[node]){
+                    bridges.push_back({it,node});
+                }
+            }else{
+                low[node]=min(low[node],low[it]);
+            }
+        }
+        
+    }
+public:
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+        vector<int>adj[n];
+        for(auto it:connections){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+        vector<int>vis(n,0);
+        int tin[n],low[n];
+        vector<vector<int>>bridges;
+        dfs(0,-1,adj,vis,tin,low,bridges);
+        return bridges;
+    }
+};
