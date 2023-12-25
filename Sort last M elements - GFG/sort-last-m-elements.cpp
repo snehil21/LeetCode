5aaf8525860ecd7d875376fb4bb6bacd3bf0a425
Snehil Kumar <snehil.kumar09@gmail.com>
2023-12-25 12:23:13 +0530

    Added solution - LeetHub

---
+++ Sort last M elements - GFG/sort-last-m-elements.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+public:
+    void sortLastMelements(vector<int>&nums, int n, int m) {
+    	// Code here
+    	sort(nums.begin(),nums.end());
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<int>nums(n+m);
+		for(auto &i: nums)
+			cin >> i;
+		Solution obj;
+		obj.sortLastMelements(nums, n, m);
+		for(auto i: nums)
+			cout << i << " ";
+		cout << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
