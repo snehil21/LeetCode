5adad9658e0ab076880934edecebd60aebaadffc
Snehil Kumar <snehil.kumar09@gmail.com>
2026-07-31 16:10:35 +0530

    LC-valid-palindrome-ii [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/valid-palindrome-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    bool isPalindrome(const string& s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+public:
+    bool validPalindrome(string s) {
+        int left = 0;
+        int right = s.size() - 1;
+
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return isPalindrome(s, left + 1, right) ||
+                       isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
