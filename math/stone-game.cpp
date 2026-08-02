75188ab00e5cc17154a0afcc9d754c038478af45
Snehil Kumar <snehil.kumar09@gmail.com>
2026-08-02 15:44:23 +0530

    LC-stone-game [Runtime: N/A; Memory: N/A]

--- math/stone-game.cpp
+++ math/stone-game.cpp
@@ -1,24 +1,4 @@
 class Solution {
 public:
-    bool stoneGame(vector<int>& piles) {
-        int alice = 0, bob = 0, start = 0, end = piles.size() - 1;
-        int turn = 0;
-        while (start < end) {
-            if (turn % 2) {
-                if (piles[start] > piles[end]) {
-                    bob += piles[start++];
-                } else {
-                    bob += piles[end--];
-                }
-            } else {
-                if (piles[start] > piles[end]) {
-                    alice += piles[start++];
-                } else {
-                    alice += piles[end--];
-                }
-            }
-            turn++;
-        }
-        return alice > bob;
-    }
+    bool stoneGame(vector<int>& piles) { return true; }
 };
